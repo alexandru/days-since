39e0b07d2f077e2c0f61aba32033ffee87e88af4
Alexandru Nedelcu <noreply@alexn.org>
2018-11-04 10:55:41 +0200

    Refactoring

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,42 @@
 module Main where
 
-import AppCmd
 import System.Exit (ExitCode(..), exitWith)
 import System.IO (hPutStrLn, stderr)
 import Text.Printf (printf)
+import Options.Applicative
+import Data.Semigroup ((<>))
 
 import qualified Data.Time as T
 import qualified Data.Time.Clock as TC
 import qualified Data.Time.Format as TF
 
 exitWithErrorMessage :: String -> ExitCode -> IO a
-exitWithErrorMessage str e =
-  hPutStrLn stderr str >> exitWith e
+exitWithErrorMessage message e =
+  hPutStrLn stderr message >> exitWith e
+
+-- |Models command line arguments
+data AppArgs = AppArgs
+  { date :: String } deriving (Show)
+
+getCmdLineArgs :: IO AppArgs
+getCmdLineArgs = execParser opts
+  where
+    appArgsParser = AppArgs
+      <$> argument str (metavar "DATE" <> help "YYYY-MM-DD format")
+    opts = info (appArgsParser <**> helper)
+      ( fullDesc
+        <> progDesc "Calculates how many days have passed since a certain date" )
 
 main :: IO ()
 main = do
   args <- getCmdLineArgs
   let dayStr = date args
-  let dayOpt = (TF.parseTimeM True TF.defaultTimeLocale "%Y-%-m-%-d" dayStr) :: Maybe T.Day
-  case dayOpt of
-    Nothing -> exitWithErrorMessage ("Error - invalid date: " <> dayStr) (ExitFailure 1)
+  let parsedDay = (TF.parseTimeM True TF.defaultTimeLocale "%Y-%-m-%-d" dayStr) :: Maybe T.Day
+  
+  case parsedDay of
+    Nothing ->
+      exitWithErrorMessage ("Error - invalid date: " <> dayStr) (ExitFailure 1)
+      
     Just day -> do
       today <- TC.getCurrentTime >>= return . T.utctDay
       let days = toInteger $ T.diffDays today day
